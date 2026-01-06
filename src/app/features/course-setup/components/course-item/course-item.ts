505e5d7650d9209c99c8f3fcf409b972dec37daf
Ousseynou <nabalma_hussein@yahoo.fr>
2026-01-06 12:40:51 -0500

    Cleansing

--- src/app/features/course-setup/components/course-item/course-item.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-course-item',
-  imports: [],
-  templateUrl: './course-item.html',
-  styleUrl: './course-item.css',
-})
-export class CourseItemComponent {}
